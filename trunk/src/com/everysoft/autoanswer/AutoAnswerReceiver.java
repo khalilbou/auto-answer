1db811b73f58213be5ea494534c5fbd94caddcdf
matth@everysoft.com <matth@everysoft.com@9ba4c95a-35b6-a26b-7bfc-4c6508f37f5e>
2010-05-05 04:06:06 +0000

    experimental support for detecting "second call" and BT headsets; need a cleaner way

--- trunk/src/com/everysoft/autoanswer/AutoAnswerReceiver.java
+++ trunk/src/com/everysoft/autoanswer/AutoAnswerReceiver.java
@@ -1,10 +1,15 @@
 package com.everysoft.autoanswer;
 
+import java.util.Set;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract.PhoneLookup;
@@ -22,6 +27,27 @@ public class AutoAnswerReceiver extends BroadcastReceiver {
 		String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
 		if (phone_state.equals(TelephonyManager.EXTRA_STATE_RINGING) && prefs.getBoolean("enabled", false)) {
+			// Check for headset restriction
+			if (prefs.getBoolean("headset_only", false)) {
+				BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+				Set<BluetoothDevice> bdevices = adapter.getBondedDevices();
+				boolean headset_found = false;
+				for (BluetoothDevice device : bdevices) {
+					//if (device.getBluetoothClass().getDeviceClass() == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE) {
+					if (device.getBluetoothClass().getDeviceClass() == 1036) {
+						headset_found = true;
+					}
+				}
+				if (!headset_found) return;
+			}
+
+			// Check for "second call" restriction
+			if (prefs.getBoolean("no_second_call", false)) {
+				AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+				if (am.getMode() == AudioManager.MODE_IN_CALL) {
+					return;
+				}
+			}			
 
 			// Check for contact restrictions
 			String which_contacts = prefs.getString("which_contacts", "all");
@@ -34,7 +60,7 @@ public class AutoAnswerReceiver extends BroadcastReceiver {
 					return;
 				}
 			}
-			
+
 			// Call a service, since this could take a few seconds
 			context.startService(new Intent(context, AutoAnswerIntentService.class));
 		}		
