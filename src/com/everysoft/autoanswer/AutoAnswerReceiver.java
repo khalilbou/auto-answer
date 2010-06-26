d550dbaad81e251d9a2cc2d488cf6ad0ee6c3363
matth <matth@everysoft.com>
2010-06-26 05:09:26 +0000

    remove bluetooth stuff -- it was too hacky -- and add support for froyo and possibly other phones

--- src/com/everysoft/autoanswer/AutoAnswerReceiver.java
+++ src/com/everysoft/autoanswer/AutoAnswerReceiver.java
@@ -1,9 +1,5 @@
 package com.everysoft.autoanswer;
 
-import java.util.Set;
-
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,20 +23,6 @@ public class AutoAnswerReceiver extends BroadcastReceiver {
 		String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
 		if (phone_state.equals(TelephonyManager.EXTRA_STATE_RINGING) && prefs.getBoolean("enabled", false)) {
-			// Check for headset restriction
-			if (prefs.getBoolean("headset_only", false)) {
-				BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-				Set<BluetoothDevice> bdevices = adapter.getBondedDevices();
-				boolean headset_found = false;
-				for (BluetoothDevice device : bdevices) {
-					//if (device.getBluetoothClass().getDeviceClass() == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE) {
-					if (device.getBluetoothClass().getDeviceClass() == 1036) {
-						headset_found = true;
-					}
-				}
-				if (!headset_found) return;
-			}
-
 			// Check for "second call" restriction
 			if (prefs.getBoolean("no_second_call", false)) {
 				AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
