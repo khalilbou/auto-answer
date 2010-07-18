f7aa85823313e1f3e4d6a6df54362ba450fb939b
matth <matth@everysoft.com>
2010-07-18 01:42:52 +0000

    added new answering routine, which will hopefully support more phones.  New release: 1.5

--- src/com/everysoft/autoanswer/AutoAnswerIntentService.java
+++ src/com/everysoft/autoanswer/AutoAnswerIntentService.java
@@ -1,5 +1,7 @@
 package com.everysoft.autoanswer;
 
+import java.lang.reflect.Method;
+import com.android.internal.telephony.ITelephony;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +9,7 @@ import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.KeyEvent;
 
 public class AutoAnswerIntentService extends IntentService {
@@ -48,13 +51,15 @@ public class AutoAnswerIntentService extends IntentService {
 			return;
 		}
 		
-		// Simulate a press of the headset button to pick up the call
-		Intent button_down = new Intent(Intent.ACTION_MEDIA_BUTTON);		
-		button_down.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
-		context.sendOrderedBroadcast(button_down, null);
-		Intent button_up = new Intent(Intent.ACTION_MEDIA_BUTTON);		
-		button_up.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
-		context.sendOrderedBroadcast(button_up, null);
+		// Answer the phone
+		try {
+			answerPhoneAidl(context);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			Log.d("AutoAnswer","Error trying to answer using telephony service.  Falling back to headset.");
+			answerPhoneHeadsethook(context);
+		}
 		
 		// Enable the speakerphone
 		if (prefs.getBoolean("use_speakerphone", false)) {
@@ -64,7 +69,34 @@ public class AutoAnswerIntentService extends IntentService {
 	}
 	
 	private void enableSpeakerPhone(Context context) {
-		AudioManager audio_manager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-		audio_manager.setSpeakerphoneOn(true);
+		AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		audioManager.setSpeakerphoneOn(true);
+	}
+	
+	private void answerPhoneHeadsethook(Context context) {
+		// Simulate a press of the headset button to pick up the call
+		Intent buttonDown = new Intent(Intent.ACTION_MEDIA_BUTTON);		
+		buttonDown.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+		context.sendOrderedBroadcast(buttonDown, "android.permission.CALL_PRIVILEGED");
+		
+		// froyo and beyond trigger on buttonUp instead of buttonDown
+		Intent buttonUp = new Intent(Intent.ACTION_MEDIA_BUTTON);		
+		buttonUp.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
+		context.sendOrderedBroadcast(buttonUp, "android.permission.CALL_PRIVILEGED");
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void answerPhoneAidl(Context context) throws Exception {
+		// Set up communication with the telephony service (thanks to Tedd's Droid Tools!)
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        Class c = Class.forName(tm.getClass().getName());
+	    Method m = c.getDeclaredMethod("getITelephony");
+	    m.setAccessible(true);
+	    ITelephony telephonyService;
+        telephonyService = (ITelephony)m.invoke(tm);
+        
+        // Silence the ringer and answer the call!
+        telephonyService.silenceRinger();
+        telephonyService.answerRingingCall();
 	}
 }
